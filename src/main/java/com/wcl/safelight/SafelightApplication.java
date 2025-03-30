38622da2bcb802fc75d7b0ebf869f2bd7f778a1b
glone0506 <glone0506@gmail.com>
2025-03-30 10:38:13 +0900

    First Commit

---
+++ src/main/java/com/wcl/safelight/SafelightApplication.java
@@ -0,0 +1,13 @@
+package com.wcl.safelight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafelightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafelightApplication.class, args);
+	}
+
+}
